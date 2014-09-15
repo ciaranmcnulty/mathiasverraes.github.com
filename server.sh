550cfbf18dda54b2d6047aa3641406c357d8fd6f
Mathias Verraes <mathias@verraes.net>
2014-09-15 16:31:49 +0200

    server

--- server.sh
+++ server.sh
@@ -2,10 +2,5 @@
 echo .
 echo "http://localhost:4000"
 echo .
-echo "Don't forget to update regularly:"
-echo "bundle install"
-echo .
 
-#jekyll serve --watch
-
-bundle exec jekyll serve --watch
+jekyll serve --watch
